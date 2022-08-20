d97fced08f9bbc50007b57663dbf232597be0a86
Low Zaar <watchmefail12@gmail.com>
2022-08-20 15:57:45 -0300

    :rocket: add XTREME WOW SO XTREME

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,21 @@
+<script src="{{asset('assets/libs/jquery/dist/jquery.min.js')}}"></script>
+<!-- Bootstrap tether Core JavaScript -->
+<script src="{{asset('assets/libs/popper.js/dist/umd/popper.min.js')}}"></script>
+<script src="{{asset('assets/libs/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('dist/js/app-style-switcher.js')}}"></script>
+<!--Wave Effects -->
+<script src="{{asset('dist/js/waves.js')}}"></script>
+<!--Menu sidebar -->
+<script src="{{asset('dist/js/sidebarmenu.js')}}"></script>
+<!--Custom JavaScript -->
+<script src="{{asset('dist/js/custom.js')}}"></script>
+<!--This page JavaScript -->
+<!--chartis chart-->
+<script src="{{asset('assets/libs/chartist/dist/chartist.min.js')}}"></script>
+<script src="{{asset('assets/libs/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js')}}"></script>
+<script src="{{asset('dist/js/pages/dashboards/dashboard1.js')}}"></script>
+<script>
+    $(document).ready(function(){
+        $(".preloader").fadeOut();
+    })
+</script>
---
+++ resources/views/components/head.blade.php
@@ -0,0 +1,14 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('assets/images/favicon.png')}}">
+    <title>@yield('title', 'Gado Manager')</title>
+    <!-- Custom CSS -->
+    <link href="{{asset('assets/libs/chartist/dist/chartist.min.css')}}" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="{{asset('dist/css/style.min.css')}}" rel="stylesheet">
+</head>
