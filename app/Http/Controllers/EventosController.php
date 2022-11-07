c8f760c843f1bfc0753c8b86a451053b462dbec5
Andrey <andreyfranca1998@gmail.com>
2022-11-06 23:01:31 -0300

    Evento de alimentação

--- app/Http/Controllers/EventosController.php
+++ app/Http/Controllers/EventosController.php
@@ -2,14 +2,64 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Flock;
+use App\Models\Food;
+use App\Models\FoodEvent;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Throwable;
 
 class EventosController extends Controller
 {
 
     public function listEventosAlimentacao()
     {
-        return view('site.eventos.eventosAlim.index');
+
+        $eventos = FoodEvent::query()
+            ->leftJoin('flocks', 'food_events.flock_id', '=', 'flocks.id')
+            ->where('flocks.company_id', Auth::user()['company_id'])
+            ->select('flocks.name as flock_name', 'food_events.start_date', 'food_events.end_date')
+            ->get()->toArray();
+
+        $alimentos = Food::query()->where('company_id', Auth::user()['company_id'])->get()->toArray();
+        $rebanhos = Flock::query()->where('company_id', Auth::user()['company_id'])->get()->toArray();
+
+        return view('site.eventos.eventosAlim.index', [
+            'eventos'   => $eventos,
+            'alimentos' => $alimentos,
+            'rebanhos'  => $rebanhos,
+        ]);
+    }
+
+    public function novoEventoAlimentacao(Request $request)
+    {
+        if ($request['start_date'] > $request['end_date']) {
+            return back()->withErrors(['A data final precisa ser maior que a data inicial!']);
+        }
+
+        try {
+            $evento = new FoodEvent();
+
+            $evento->flock_id = $request['rebanho'];
+            $evento->description = $request['descricao'];
+            $evento->start_date = $request['start_date'];
+            $evento->end_date = $request['end_date'];
+
+            $evento->save();
+
+            foreach ($request['alimentos'] as $alimento) {
+                DB::table('food_event_items')->insert([
+                    'event_id' => $evento->id,
+                    'food_id' => $alimento
+                ]);
+            }
+
+        } catch (Throwable $t) {
+            return back()->withErrors(['Erro ao cadastrar evento!']);
+        }
+
+        return back()->with('success', 'Evento cadastrado com sucesso');
     }
 
     public function listEventosMedicacao()
@@ -21,5 +71,5 @@ class EventosController extends Controller
     {
         return view('site.eventos.eventosVac.index');
     }
-    
+
 }
